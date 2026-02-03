cd6a1dc600fc3d86b560f1cc98cb1b2c62489268
RishiVadde <143945231+RishiVadde@users.noreply.github.com>
2026-02-03 10:40:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.get('/')
+@app.get("/")
 def home():
-    return 'Hello! Deployed by Jenkins on localhost ✅'
+    return "Hello from Jenkins"
 
-@app.get('/health')
+@app.get("/health")
 def health():
-    return {'status': 'ok'}, 200
+    return {"status": "ok"}, 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
